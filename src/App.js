1883fef6fab4d25e65cc90557f52893890673eda
shinmikouji <sinkou83@gmail.com>
2022-08-16 22:55:01 +0900

    field component recreate

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Field from './components/Field';
 import ManipulationPanel from './components/ManipulationPanel';
 import Navigation from './components/Navigation';
 import Title from './components/Title';
+import { initField } from './utils';
+
+const fields = initField(35)
 
 function App() {
   return (
@@ -12,7 +15,7 @@ function App() {
         <Navigation/>
       </header>
       <main className='main'>
-        <Field/>
+        <Field fields={fields} />
       </main>
       <footer className='footer'>
         <Button/>
---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+export const initField = (fieldSize) => {
+  const fields = [];
+  for(let i = 0; i < fieldSize; i++) {
+    const cols = new Array(fieldSize).fill("");
+    fields.push(cols);
+  }
+  fields[17][17] = 'snake'
+  return fields
+}
