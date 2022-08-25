f732195824afc7b8794e8b4313b0b413da55d3ab
regaliz-develop <sales.regaliz@gmail.com>
2022-08-26 00:10:50 +0900

    start-and-over

--- src/App.js
+++ src/App.js
@@ -1,65 +1,104 @@
-import {useState, useEffect} from 'react';
-import Button from './components/Button';
-import Field from './components/Field';
-import ManipulationPanel from './components/ManipulationPanel';
-import Navigation from './components/Navigation';
-import Title from './components/Title';
-import { initField } from './utils';
+import { useState, useEffect } from "react";
+import Button from "./components/Button";
+import Field from "./components/Field";
+import ManipulationPanel from "./components/ManipulationPanel";
+import Navigation from "./components/Navigation";
+import Title from "./components/Title";
+import { initField } from "./utils";
 
-const initialPosition = {x: 17, y: 17};
+const initialPosition = { x: 17, y: 17 };
 const initialValues = initField(35, initialPosition);
-const defaultInterval = 100
+const defaultInterval = 100;
+const GameStatus = Object.freeze({
+  init: "init",
+  playing: "playing",
+  suspended: "suspended",
+  gameover: "gameover",
+});
 
-let timer = undefined
+let timer = undefined;
 
 const unsubscribe = () => {
-  if(!timer) {
-    return
+  if (!timer) {
+    return;
   }
-  clearInterval(timer)
-}
+  clearInterval(timer);
+};
+
+const isCollision = (fieldSize, position) => {
+  if (position.y < 0 || position.x < 0) {
+    return true;
+  }
+
+  if (position.y > fieldSize - 1 || position.x > fieldSize - 1) {
+    return true;
+  }
+
+  return false;
+};
 
 function App() {
   const [fields, setFields] = useState(initialValues);
   const [position, setPosition] = useState();
-  const [tick, setTick] = useState(0)
+  const [status, setStatus] = useState(GameStatus.init);
+  const [tick, setTick] = useState(0);
 
   useEffect(() => {
-    setPosition(initialPosition)
-     // ゲームの中の時間を管理する
+    setPosition(initialPosition);
+    // ゲームの中の時間を管理する
     timer = setInterval(() => {
-      setTick(tick => tick + 1)
+      setTick((tick) => tick + 1);
     }, defaultInterval);
-    return unsubscribe
+    return unsubscribe;
   }, []);
 
   useEffect(() => {
-    if (!position) {
-      return
+    if (!position || status !== GameStatus.playing) {
+      return;
     }
-    goUp()
-  }, [tick])
+    const canContinue = goUp();
+    if (!canContinue) {
+      setStatus(GameStatus.gameover);
+    }
+  }, [tick]);
+
+  const onStart = () => setStatus(GameStatus.playing);
+
+  const onRestart = () => {
+    timer = setInterval(() => {
+      setTick(tick => tick + 1)
+    }, defaultInterval)
+    setStatus(GameStatus.init)
+    setPosition(initialPosition)
+    setFields(initField(35, initialPosition))
+  }
 
   const goUp = () => {
-    const {x, y} = position;
-    const nextY = Math.max(y -1, 0);
-    fields[y][x] = '';
-    fields[nextY][x] = 'snake';
-    setPosition({x, y: nextY});
-    setFields(fields)
-  }
+    const { x, y } = position;
+    const nextY = Math.max(y - 1, 0);
+    const newPosition = { x, y: y -1 }
+    if (isCollision(fields.length, newPosition)) {
+      unsubscribe()
+      return false
+    }
+    fields[y][x] = "";
+    fields[newPosition.y][x] = 'snake'
+    setPosition(newPosition)
+    setFields(fields);
+    return true
+  };
   return (
     <div className="App">
-      <header className='header'>
-        <Title/>
-        <Navigation/>
+      <header className="header">
+        <Title />
+        <Navigation />
       </header>
-      <main className='main'>
+      <main className="main">
         <Field fields={fields} />
       </main>
-      <footer className='footer'>
-        <Button/>
-        <ManipulationPanel/>
+      <footer className="footer">
+      <Button status={status} onStart={onStart} onRestart={onRestart}/>
+        <ManipulationPanel />
       </footer>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ status, onStart, onRestart }) => {
   return (
     <div className='button'>
-      <button>
-        start
+      <button onClick={onStart}>
+      {
+        status === "gameover" ?
+        <button onClick={onRestart}>gameover</button>
+        :
+        <button onClick={onStart}>start</button>
+      }
       </button>
     </div>
   )
